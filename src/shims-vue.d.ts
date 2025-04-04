0f3ad2d661d782b9816d1a2537f7976178bcf8ad
arsen <arsen.yeghiazarian@gmail.com>
2025-04-04 17:41:37 +0400

    env.d.ts config add

--- src/shims-vue.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="vite/client" />
-
-declare module '*.vue' {
-  import { DefineComponent } from 'vue'
-  const component: DefineComponent<object, object, unknown>
-  export default component
-}
