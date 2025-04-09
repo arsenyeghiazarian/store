91ceef457cf87ac3005bc2d73be78edab26793f5
arsen <arsen.yeghiazarian@gmail.com>
2025-04-09 15:30:20 +0400

    document title add in routing

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,36 +1,72 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+// Preload the home page component
+const Home = () => import('@/views/CategoriesList.vue')
+
 const routes = [
   {
     path: '/',
     name: 'Home',
-    component: () => import('@/views/CategoriesList.vue')
+    component: Home,
+    meta: {
+      title: 'Home',
+      keepAlive: true
+    }
   },
   {
     path: '/category/:id',
     name: 'CategoryPage',
-    component: () => import('@/views/CategoryPage.vue')
+    component: () => import('@/views/CategoryPage.vue'),
+    meta: {
+      title: 'Category',
+      keepAlive: true
+    }
   },
   {
     path: '/product/:id',
     name: 'ProductDetails',
-    component: () => import('@/views/ProductDetails.vue')
+    component: () => import('@/views/ProductDetails.vue'),
+    meta: {
+      title: 'Product Details',
+      keepAlive: false
+    }
   },
   {
     path: '/cart',
     name: 'CartPage',
-    component: () => import('@/views/CartPage.vue')
+    component: () => import('@/views/CartPage.vue'),
+    meta: {
+      title: 'Shopping Cart',
+      keepAlive: true
+    }
   },
   {
     path: '/:pathMatch(.*)*',
     name: 'NotFound',
-    component: () => import('@/views/PageNotFound.vue')
+    component: () => import('@/views/PageNotFound.vue'),
+    meta: {
+      title: 'Page Not Found',
+      keepAlive: false
+    }
   },
 ]
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    } else {
+      return { top: 0 }
+    }
+  }
+})
+
+// Update page title
+router.beforeEach((to, from, next) => {
+  document.title = `${to.meta.title} | Store` || 'Store'
+  next()
 })
 
 export default router
